a2ba4dd462b938168aaa5be9bf2b7977cee44d90
VincentFvan <994992433@qq.com>
2022-09-20 17:17:50 +0800

    finish create RR in the fabric cache

--- errmsg/errmsg.go
+++ errmsg/errmsg.go
@@ -8,7 +8,7 @@ var codeMsg = map[int]string{
 	http.StatusBadRequest:          "参数错误",
 	http.StatusUnauthorized:        "签名验证失败",
 	http.StatusNotFound:            "记录不存在",
-	http.StatusForbidden:           "权限错误，g",
+	http.StatusForbidden:           "权限错误",
 	http.StatusInternalServerError: "服务器错误",
 }
 
